7f0dfffd0c60e06fcb7d857a06bf21f4f00af34a
3x7r3m157 <austin@hackernest.com>
2016-11-17 15:51:57 -0600

    assignment-28: completed...booyakasha, big-up-yo'self

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let(:answer) { Answer.create!(body: "Answer Body") }
+
+  describe "attributes" do
+    it "has a body attribute" do
+      expect(answer).to have_attributes(body: "Answer Body")
+    end
+  end
+end
